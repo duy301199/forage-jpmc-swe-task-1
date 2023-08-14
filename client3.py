8c4c049b78d7989213956387e93528d048fd9602
Duy Nguyen <duy301199@csu.fullerton.edu>
2023-08-13 18:12:11 -0700

    update

--- client3.py
+++ client3.py
@@ -42,8 +42,7 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if (price_b == 0):
-    # when price_b is 0, avoid throwing ZeroDiv Error
+    if (price == 0):
         return
     return price_a / price_b
 
@@ -55,10 +54,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices = {} #Create a dictionary to store stock prices
+        prices ={} #Create a dictionary to store stock prices
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % getRatio(prices["ABC"],prices["DEF"]))
